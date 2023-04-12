ef24983970fdfca9bf859df7e39a7c0be053847f
jay-isampelliwar <isampelliwar79@gmail.com>
2023-04-12 18:04:20 +0530

    Added TodoBloc on UI

--- lib/features/todo/bloc/todo_bloc.dart
+++ lib/features/todo/bloc/todo_bloc.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
 
@@ -6,8 +8,36 @@ part 'todo_state.dart';
 
 class TodoBloc extends Bloc<TodoEvent, TodoState> {
   TodoBloc() : super(TodoInitial()) {
-    on<TodoEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+    on<TodoDateCardButtonClickedActionEvent>(
+        todoDateCardButtonClickedActionEvent);
+    on<TodoTimeCardButtonClickedActionEvent>(
+        todoTimeCardButtonClickedActionEvent);
+
+    on<TodoBusinessChipSelectEvent>(todoBusinessChipSelectEvent);
+    on<TodoPersonalChipSelectEvent>(todoPersonalChipSelectEvent);
+    on<TodoUrgentChipSelectEvent>(todoUrgentChipSelectEvent);
+    on<TodoAddTaskButtonClickedActionEvent>(
+        todoAddTaskButtonClickedActionEvent);
+    on<TodoCloseButtonClickedActionEvent>(todoCloseButtonClickedActionEvent);
   }
+
+  FutureOr<void> todoDateCardButtonClickedActionEvent(
+      TodoDateCardButtonClickedActionEvent event, Emitter<TodoState> emit) {}
+
+  FutureOr<void> todoTimeCardButtonClickedActionEvent(
+      TodoTimeCardButtonClickedActionEvent event, Emitter<TodoState> emit) {}
+
+  FutureOr<void> todoBusinessChipSelectEvent(
+      TodoBusinessChipSelectEvent event, Emitter<TodoState> emit) {}
+
+  FutureOr<void> todoPersonalChipSelectEvent(
+      TodoPersonalChipSelectEvent event, Emitter<TodoState> emit) {}
+  FutureOr<void> todoUrgentChipSelectEvent(
+      TodoUrgentChipSelectEvent event, Emitter<TodoState> emit) {}
+
+  FutureOr<void> todoAddTaskButtonClickedActionEvent(
+      TodoAddTaskButtonClickedActionEvent event, Emitter<TodoState> emit) {}
+
+  FutureOr<void> todoCloseButtonClickedActionEvent(
+      TodoCloseButtonClickedActionEvent event, Emitter<TodoState> emit) {}
 }
--- lib/features/todo/bloc/todo_event.dart
+++ lib/features/todo/bloc/todo_event.dart
@@ -2,3 +2,17 @@ part of 'todo_bloc.dart';
 
 @immutable
 abstract class TodoEvent {}
+
+class TodoDateCardButtonClickedActionEvent extends TodoEvent {}
+
+class TodoTimeCardButtonClickedActionEvent extends TodoEvent {}
+
+class TodoBusinessChipSelectEvent extends TodoEvent {}
+
+class TodoPersonalChipSelectEvent extends TodoEvent {}
+
+class TodoUrgentChipSelectEvent extends TodoEvent {}
+
+class TodoAddTaskButtonClickedActionEvent extends TodoEvent {}
+
+class TodoCloseButtonClickedActionEvent extends TodoEvent {}
